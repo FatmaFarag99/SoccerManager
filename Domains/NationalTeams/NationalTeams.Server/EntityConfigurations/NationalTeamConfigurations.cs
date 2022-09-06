75611ae490a0fd226f6827df7ba457adde31f617
FatmaFarag <fatmafarag17799@gmail.com>
2022-09-06 00:57:10 -0700

    National Team

--- Domains/NationalTeams/NationalTeams.Server/EntityConfigurations/NationalTeamConfigurations.cs
+++ Domains/NationalTeams/NationalTeams.Server/EntityConfigurations/NationalTeamConfigurations.cs
@@ -5,5 +5,9 @@ public class NationalTeamConfigurations : BaseEntityConfiguration<NationalTeam>
     {
         base.Configure(builder);
         builder.Property(e => e.Name).IsRequired();
+        builder.Property(e => e.NickName).IsRequired();
+        builder.Property(e => e.FifaCode).IsRequired();
+        builder.Property(e => e.MostCaps).IsRequired();
+        builder.Property(e => e.Association).IsRequired();
     }
 }
--- Domains/NationalTeams/NationalTeams.Shared/Validators/NationalTeamValidator.cs
+++ Domains/NationalTeams/NationalTeams.Shared/Validators/NationalTeamValidator.cs
@@ -4,5 +4,10 @@ public class NationalTeamValidator : AbstractValidator<NationalTeamViewModel>
 	public NationalTeamValidator()
 	{
 		RuleFor(x => x.Name).NotEmpty().WithMessage("Name is Required");
+		RuleFor(x => x.NickName).NotEmpty().WithMessage("NickName is Required");
+		RuleFor(x => x.MostCaps).NotEmpty().WithMessage("MostCaps is Required");
+		RuleFor(x => x.FifaCode).NotEmpty().WithMessage("FifaCode is Required");
+		RuleFor(x => x.Association).NotEmpty().WithMessage("Association is Required");	
+		RuleFor(x => x.Confedration).NotEmpty().WithMessage("Confedration is Required");	
 	}
 }
