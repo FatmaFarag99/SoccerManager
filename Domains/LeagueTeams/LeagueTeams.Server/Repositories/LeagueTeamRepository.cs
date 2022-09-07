5ef7a33ce0c6709fc974b52324f21dfc6d0e9390
FatmaFarag <fatmafarag17799@gmail.com>
2022-09-07 14:32:11 -0700

    Includes

--- Domains/LeagueTeams/LeagueTeams.Server/Repositories/LeagueTeamRepository.cs
+++ Domains/LeagueTeams/LeagueTeams.Server/Repositories/LeagueTeamRepository.cs
@@ -3,5 +3,20 @@ public class LeagueTeamRepository : BaseRepository<LeagueTeam> , ILeagueTeamRepo
 {
     public LeagueTeamRepository(DbContext dbContext) : base(dbContext)
     {
+
+    }
+
+    public override async Task<List<LeagueTeam>> GetAllAsync()
+    {
+        return await _table
+            .Include(c => c.Players)
+            .ToListAsync();
+    }
+
+    public override async Task<LeagueTeam> GetByIdAsync(Guid id)
+    {
+        return await _table
+            .Include(c => c.Players)
+            .FirstOrDefaultAsync(c => c.Id == id);
     }
 }
--- Domains/NationalTeams/NationalTeams.Server/Repositories/NationalTeamRepository.cs
+++ Domains/NationalTeams/NationalTeams.Server/Repositories/NationalTeamRepository.cs
@@ -4,4 +4,18 @@ public class NationalTeamRepository : BaseRepository<NationalTeam> , INationalTe
     public NationalTeamRepository(DbContext dbContext) : base(dbContext)
     {
     }
+
+    public override async Task<List<NationalTeam>> GetAllAsync()
+    {
+        return await _table
+            .Include(c => c.Players)
+            .ToListAsync();
+    }
+
+    public override async Task<NationalTeam> GetByIdAsync(Guid id)
+    {
+        return await _table
+            .Include(c => c.Players)
+            .FirstOrDefaultAsync(c => c.Id == id);
+    }
 }
